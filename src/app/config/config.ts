51f9df2bc6d3c685ce39a2ce374a0602b5950844
glazaror <glazaror@gmail.com>
2020-12-04 06:49:58 -0500

    Updating customer microservice backend url deployed in heroku

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
-//export const URL_BACKEND = 'https://spring-boot2-heroku.herokuapp.com';
-export const URL_BACKEND = 'http://localhost:8080';
+export const URL_BACKEND = 'https://cliente-microservice.herokuapp.com';
+//export const URL_BACKEND = 'http://localhost:8080';
