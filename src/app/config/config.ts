4c6fb5e44ce16fc318e1a649ac2b6e132878c0db
glazaror <glazaror@gmail.com>
2020-12-07 07:44:13 -0500

    Setting https access to cliente-microservice

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
 //export const URL_BACKEND = 'https://cliente-microservice.herokuapp.com';
-export const URL_BACKEND = 'http://microservices-glazaror-test-server.southcentralus.cloudapp.azure.com:8090';
+export const URL_BACKEND = 'https://microservices-glazaror-test-server.southcentralus.cloudapp.azure.com';
 //export const URL_BACKEND = 'http://localhost:8090';
